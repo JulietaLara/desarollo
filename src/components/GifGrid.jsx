2384387d10905b5e62ce438fd0b1ebb1e7b152f5
JulietaLara <80423327+JulietaLara@users.noreply.github.com>
2022-09-06 19:58:13 -0500

    martes 09 06

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react"
+import { getGifs } from "./getGifs"
 //import { getGifs} from "../helpers/getGifs"
 
 
 export const GifGrid = ({category}) => {
 
+    const [sal, set] = useState([])
+
     useEffect( () => {
         console.log(category)
     }, []) //imprime solo cada que se agrega un componente
 
+  
+    const newFunction = async(  ) => {
+        var g = await getGifs(category) //se pone await porque es async 
+        console.log(g)
+        set(g)
+
+    }
 
     return (
         <>
         <h3> { category }  </h3>
         <p> Hello World</p>
+        <button onClick={() => newFunction()}> Bim barabum mr.worldwide </button>
+        <p>  {JSON.stringify(sal)}</p>
         </>
 
     )
