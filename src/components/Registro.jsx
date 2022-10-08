97c54cb5fd3325807d603d7bf63c8c8d068e5360
JulietaLara <80423327+JulietaLara@users.noreply.github.com>
2022-10-08 09:14:57 -0500

    Firebase 3
    ultimo de firebase hecho en clase

---
+++ src/components/Registro.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { UseForm } from "../hooks/useForm";
+import { registerAuth, RegisterAuth } from "../store/slices/auth/Thunks"
+
+export const Registro = () => {
+    const dispatch = useDispatch()
+
+    const { email, password, onInputChange, formState } = UseForm({
+        email: "julieta.lara@uao.edu.co",
+        password: "pass"
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        console.log(formState)
+        dispatch( registerAuth( email, password ))
+    }
+
+    useEffect(() => {
+        onAuthStateChanged( auth, async( user ) => {
+            if ( !user ) return dispatch( logout() );
+
+            dispatch( register( {email: user.email} ) ) 
+        })
+    }, [])
+
+    return (
+        <>
+            <h1>Registro</h1>
+            <hr />
+            <form onSubmit={(event) => onSubmit(event)}>
+                <input name="email" type="email" onChange={ (event) => onInputChange(event)} value={email} />
+                <input name="password" type="password" onChange={ (event) => onInputChange(event)} value={password} />
+
+                <button type="submit"> Registro </button>
+            </form>
+        </>
+    )
+}
